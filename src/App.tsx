4143237a97b55c1ab9a9877b3278666ec8c401f3
Gabriel Antony <gmamanican@unsa.edu.pe>
2025-01-10 11:13:53 -0500

    add handlers for input and logic for output

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import { useState } from "react";
-import "./App.css";
+import { Matriz } from "./components/Matriz";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [matrizInput, setMatrizInput] = useState("");
+
+  const [matrizResultado, setMatrizResultado] = useState<string[][]>([[]]);
+  const [matrizOriginal, setMatrizOriginal] = useState<string[][]>([[]]);
+
+  const handleRotation = () => {
+    if (!matrizInput) {
+      alert("Debes ingresar una matriz");
+      return;
+    }
+
+    try {
+      const matriz = JSON.parse(matrizInput);
+      if (!Array.isArray(matriz)) {
+        alert("El elemento ingresado no es una matriz");
+        return;
+      }
+      // Número de filas
+      const n = matriz.length;
+
+      for (let i = 0; i < n; i++) {
+        // Verifica que cada fila sea matriz valida y tenga la misma cantidad de columnas
+        if (!Array.isArray(matriz[i]) || matriz[i].length !== n) {
+          alert("Debes ingresar una matriz NxN");
+          return;
+        }
+      }
+      setMatrizOriginal(matriz);
+
+      const rotatedMatriz = matriz.map((_, i) =>
+        matriz.map((_, j) => matriz[j][n - 1 - i])
+      );
+      setMatrizResultado(rotatedMatriz);
+    } catch (error) {
+      console.log(error);
+      alert("Debes ingresar una matriz válida");
+    }
+  };
 
   return (
-    <>
-      <h1 className="text-blue-950">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-pink-950">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="flex flex-col items-center m-12">
+      <h1 className="text-blue-950 text-4xl font-semibold">
+        Rotación 90° matriz NxN
+      </h1>
+      <div className="my-5">
+        <label htmlFor="matrizInput" className="font-bold">
+          JSON Matriz Input:{" "}
+        </label>
+        <input
+          className="border px-2 py-1 ml-2"
+          type="text"
+          placeholder="Ingrese una matriz NxN"
+          id="matrizInput"
+          value={matrizInput}
+          onChange={(e) => setMatrizInput(e.target.value)}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button
+        className="py-2 px-12 rounded-lg border bg-blue-600 text-white font-semibold hover:bg-blue-700"
+        onClick={handleRotation}
+      >
+        Rotar
+      </button>
+      {matrizResultado && (
+        <div className="flex flex-row my-4">
+          <Matriz label="Matriz ingresada" matriz={matrizOriginal} />
+          <Matriz label="Resultado" matriz={matrizResultado} />
+        </div>
+      )}
+    </div>
   );
 }
 
